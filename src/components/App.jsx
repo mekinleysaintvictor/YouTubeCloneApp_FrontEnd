ca21f2d22ae6abe0ed4355542fd6880a607244bb
unknown <mekinleydsaintvictor@outlook.com>
2021-09-16 15:53:36 -0400

    Added getAllComments function to APP component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Comments from './Comments/Comments';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-
+            videos: [],
+            videoCommentsFromAPI: []
          }
     }
+
+    componentDidMount(){
+        this.getAllComments();
+    }
+
+    async getAllComments(){
+        let response = await axios.get('http://127.0.0.1:8000/video/');
+        console.log(response.data[0].comment);
+        console.log(response.data);
+        this.setState({
+            videoCommentsFromApI: response.data
+        })
+    }
+
+
+
     render() { 
         return ( 
             <div>
                 <h1>Hello World</h1>
+                <h1>
+                    {this.state.videoCommentsFromAPI.map(remark => {
+                        return <h2>{remark.comment}</h2>
+                    })}
+                </h1>
             </div>
          );
     }
---
+++ src/components/Comments/Comments.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Comments = (props) => {
+    return ( 
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Comment</th>
+                </tr>
+            </thead>
+        </table>
+     );
+}
+ 
+export default Comments;
